6ea2e58315009ee5474f0d044f2c9e716b75788f
Akbar <n.akbarjon05@gmail.com>
2025-09-04 13:21:07 +0300

    smthing

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,13 @@ app.use((req, res, next) => {
   next();
 });
 app.use(cors({
-  origin: allowedOrigins,
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
